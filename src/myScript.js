1b6e09a505be18822e37c69530410d82551309a4
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-02-05 13:41:04 +0530

    tore Multiple Values in one Variable using JavaScript Arrays

--- src/myScript.js
+++ src/myScript.js
@@ -180,3 +180,6 @@ const myAdverb = "quickly";
 // Only change code below this line
 const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Change this line
 // Only change code above this line
+
+// Only change code below this line
+const myArray = ["string",10];
