47f70e35bb24c15e4c70ecfbfd064f23cbc71d9f
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-02-05 11:34:03 +0530

    Understanding Uninitialized Variables

--- src/myScript.js
+++ src/myScript.js
@@ -1,14 +1,9 @@
-// This is an in-line comment.
-
-/* This is a
-multi-line comment */
-
-var myName;
-
-// Setup
-var a = 9;
-
-var myFirstName = "your name";
-var myLastName = "your name";
-
 // Only change code below this line
+var a= 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
