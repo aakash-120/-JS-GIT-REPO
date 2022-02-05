3f661590d183d5e6773f7a7d8021f61e24403b99
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-02-05 15:26:54 +0530

    Understanding Undefined Value returned from a Function

--- src/myScript.js
+++ src/myScript.js
@@ -314,4 +314,23 @@ myOutfit();
 console.log(myOutfit());
 
 
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+
+
   
