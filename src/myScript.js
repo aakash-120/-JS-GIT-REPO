59b5d38e9ec9e8cf055226dc4d87f32105d50591
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-02-05 18:09:09 +0530

    Testing Objects for Properties

--- src/myScript.js
+++ src/myScript.js
@@ -921,3 +921,15 @@ function phoneticLookup(val) {
   }
   
   phoneticLookup("charlie");
+
+
+  function checkObj(obj, checkProp) {
+    // Only change code below this line
+     if (obj.hasOwnProperty(checkProp)) {
+      return obj[checkProp];
+    } else {
+      return "Not Found";
+    }
+    return "Change Me!";
+    // Only change code above this line
+  }
