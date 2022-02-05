0dc1787b9bb0c2aa34fb83cde679ecbe616d1985
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-02-05 14:22:08 +0530

    Passing Values to Functions with Arguments

--- src/myScript.js
+++ src/myScript.js
@@ -244,3 +244,12 @@ function reusableFunction() {
   }
   
   reusableFunction();
+
+  
+  function functionWithArgs(param1, param2) {
+    console.log(param1+param2);
+  }
+  
+  functionWithArgs(1,2);
+  functionWithArgs(7,9);
+  
