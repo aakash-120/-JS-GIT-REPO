dc5f21e7cfc40ac3bcc8fe270bc481bffefb2ee6
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-02-05 17:07:46 +0530

    Returning Boolean Values from Functions

--- src/myScript.js
+++ src/myScript.js
@@ -744,3 +744,12 @@ function caseInSwitch(val) {
   }
   
   chainToSwitch(7);
+
+
+  function isLess(a, b) {
+    // Only change code below this line
+  return a<b;
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
