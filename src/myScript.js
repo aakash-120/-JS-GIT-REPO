90a0850ab92a7d9612a76561b051b98188e3faa7
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-02-05 15:47:21 +0530

    Use Conditional Logic with If Statements

--- src/myScript.js
+++ src/myScript.js
@@ -370,3 +370,18 @@ function nextInLine(arr, item) {
   }
   var bool = welcomeToBooleans();
   console.log(bool);
+
+
+  function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+   if (wasThatTrue) {
+      return "Yes, that was true";
+    }
+     if (!wasThatTrue) {
+      return "No, that was false";
+    }
+  
+  
+    // Only change code above this line
+  
+  }
